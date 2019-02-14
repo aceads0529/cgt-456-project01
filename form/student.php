7bb90007e6277e9d76a7b453e2cf0c988a690bb5
turne248 <turne248@purdue.edu>
2019-02-13 20:46:28 -0500

    added menu pages to toggle thru

---
+++ form/student.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; UTF-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=.75">
+    	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<title>CGT 456 | Project 1</title> 
+	</head>
+	<body>
+		<header>
+			<img src="https://polytechnic.purdue.edu/sites/default/files/files/MM-Polyt-H-BG-RGB.png">
+			
+
+		</header>
+
+		<section id="content">
+
+
+			<div id="formContainer">
+				<?php include 'form/studentForm.php'; ?>
+			</div>
+
+		</section>
+	</body>
+</html>
